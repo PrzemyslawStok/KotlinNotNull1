12dda263f21a1e01d92979e10206429c9572ac1c
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-05 14:55:08 +0200

    cd ..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,9 @@
+class Student(val name: String, val surname: String){
+    override fun toString(): String {
+        return "Student(name='$name', surname='$surname')"
+    }
+}
+
 fun main() {
     var i = 10
     var name: String? = "Przemysław"
@@ -12,7 +18,23 @@ fun main() {
     println(x)
 
     var y = 10.0
-    //y = x
+
+    if(x!=null)
+        y = x
+    else
+        y = 0.0
+
+    y = x?:0.0
+
+    var student0 = Student("Przemysław","Stokłosa")
+    println(student0)
+
+    for(i in 0..20){
+        println(getStudent(i))
+    }
+
+    student0 = getStudent(5)?:Student("Domyślny","Student")
+    println(student0)
 }
 
 fun div(x: Double, y: Double):Double?{
@@ -24,4 +46,11 @@ fun div(x: Double, y: Double):Double?{
 
 fun getNumber(textNumber: String):Double?{
     return textNumber.toDoubleOrNull()
+}
+
+fun getStudent(id: Int):Student?{
+    if(id<10)
+        return Student("Przemysław","Stokłosa_$id")
+    else
+        return null
 }
