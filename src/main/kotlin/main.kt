c3e83a20ec76a57c7bbc51a62a2463963bf17a1e
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-06-05 14:35:15 +0200

    cd ..

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    var i = 10
+    var name: String? = "Przemysław"
+
+    name = null
+
+    var z:Double? = 0.0
+    z = div(10.0,0.0)
+    println(z)
+
+    val x = getNumber("10.0")
+    println(x)
+
+    var y = 10.0
+    //y = x
+}
+
+fun div(x: Double, y: Double):Double?{
+    if(y==0.0){
+        return null
+    }
+    return x/y
+}
+
+fun getNumber(textNumber: String):Double?{
+    return textNumber.toDoubleOrNull()
 }
