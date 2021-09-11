4ae129670f5888c51ad78c0040afe26536dc3643
Przemyslaw Stoklosa <przemyslaw.stoklosa@gmail.com>
2021-09-11 14:34:39 +0200

    cd..

--- src/main/kotlin/zadania1.kt
+++ src/main/kotlin/zadania1.kt
@@ -2,5 +2,18 @@ class zadania1 {
 }
 
 fun main(){
-    
+    var i:Int = 10
+    var j:Int? = 10
+
+    i = add(2,10)
+    j = add(2,10)
+
+    j = null
+
+}
+
+fun add(a: Int, b: Int):Int?{
+    if(a>b)
+        return null
+    return a+b
 }
